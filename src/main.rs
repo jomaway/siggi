c7374259ede65f03f84684c6307c108a70ce36e8
jomaway <dev@jomaway.de>
2022-02-24 18:14:41 +0100

    Added examples

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 
-use siggi::{model::{Signal, signal::{Wave, Clock, SignalGenerator, Level}, Diagram, Lane, markers::{Label, Line}, utils::Color}, compose::Compositor};
+use std::str::FromStr;
+
+use siggi::{model::{Signal, signal::{Wave, Clock, SignalGenerator, Level}, Diagram, Lane, markers::{Label, Line, Marker}, utils::Color}, compose::Compositor};
 
 use clap::Parser as ClapParser;
 
@@ -60,6 +62,9 @@ fn main() {
 
     svg::save("target/siggi.svg", &doc).expect("Could not save the diagram.");
 
+
+    svg::save("target/test.svg", &comp.compose(&generate_manchester_example())).expect("Could not save the test diagram.");
+
 }
 
 #[allow(unused)]
@@ -73,12 +78,63 @@ fn generate_test_diagram() -> Diagram {
 
     let mut d1 = Diagram::new("Simple siggi diagram");
 
-    d1.add(Lane::new(nclk).add_label(Label::default()).clone());
-    d1.add(Lane::new(s1).add_marker(Line::default()).clone());
+    let markerline = Line::new(1.5,true,1.2, Color::Red);
+    d1.add(Lane::new(nclk).add_marker(markerline)
+        .with_label_at("rising edge", 1.5).clone());
+    d1.add(Lane::new(s1).add_marker(markerline)
+        .with_label_at("0", 0.5)
+        .with_label_at("1", 1.5)
+        .with_label_at("0", 2.5)
+        .with_label_at("1", 3.5)
+        .clone());
 
-    let mut l3 = Lane::new(pclk.to_signal());
-    l3.label_at(String::from("lab"), 2.3);
-    l3.mark_at(1.8);
-    //d1.add(l3);
+    let mut l3 = Lane::new(pclk.to_signal().color(Color::Blue).clone());
+    l3.label_at(String::from("&lt;- t -&gt;"), 1.5);
+    l3.mark_at(1.0);
+    l3.mark_at(2.0);
+    d1.add(l3);
+    d1
+}
+
+#[allow(unused)]
+fn generate_manchester_example() -> Diagram {
+    let clk = Clock::negativ(11);
+    let clk = clk.to_signal().color(Color::Yellow).clone();
+
+    let mut s1= Signal::default();
+    s1.name("Data").wave(Wave::from_str("hlhllhhhllh").unwrap()).color(Color::Red);
+
+    let mut s2 = Signal::default();
+    s2.name("Manchester").wave(Wave::from_str("ududduuuddu").unwrap()).color(Color::Blue);
+
+    let labels = vec![
+        Label::new("1").at(0.5).clone(),
+        Label::new("0").at(1.5).clone(),
+        Label::new("1").at(2.5).clone(),
+        Label::new("0").at(3.5).clone(),
+        Label::new("0").at(4.5).clone(),
+        Label::new("1").at(5.5).clone(),
+        Label::new("1").at(6.5).clone(),
+        Label::new("1").at(7.5).clone(),
+        Label::new("0").at(8.5).clone(),
+        Label::new("0").at(9.5).clone(),
+        Label::new("1").at(10.5).clone()
+    ];
+
+    let markerline = Line::new(1.5,true,1.2, Color::Red);
+
+    let mut d1 = Diagram::new("Manchester Encoding");
+
+    let l1 = Lane::new(clk).add_marker(markerline).clone();
+    let mut l2 = Lane::new(s1).add_marker(markerline).clone();
+    let mut l3 = Lane::new(s2).add_marker(markerline).clone();
+        
+    l2.labels.extend(labels.clone());
+    l3.labels.extend(labels);
+
+    d1.add(l1);
+    d1.add(l2);
+    d1.add(l3);
+
     d1
 }

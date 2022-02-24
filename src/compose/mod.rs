386ea62f7dd7c73927b8741971cccede71b4537f
jomaway <dev@jomaway.de>
2022-02-24 10:09:23 +0100

    Added compositor

--- src/compose/mod.rs
+++ src/compose/mod.rs
@@ -1,11 +1,14 @@
 // compose/mod.rs
+use svg::{self, node::element::{Group, Text, Path, Rectangle, Line, path::Data}, Node};
+
+use crate::model::{Diagram, markers::{Label, TextAnchor}, Lane, utils::Color, Signal, signal::Level};
 
 // Constants
 const PADDING: f64 = 30.0;
-const PADDING_TOP: f64 = PADDING;
-const PADDING_BOTTOM: f64 = PADDING;
-const PADDING_LEFT: f64 = PADDING;
-const PADDING_RIGHT: f64 = PADDING;
+const _PADDING_TOP: f64 = PADDING;
+const _PADDING_BOTTOM: f64 = PADDING;
+const _PADDING_LEFT: f64 = PADDING;
+const _PADDING_RIGHT: f64 = PADDING;
 const HEADER_HEIGHT: f64 = 25.0;  // if no title set to 0;
 const FOOTER_HEIGHT: f64 = 0.0;
 
@@ -16,9 +19,242 @@ const WAVE_PADDING_TOP: f64 = WAVE_PADDING;
 const WAVE_PADDING_BOTTOM: f64 = WAVE_PADDING;
 const LANE_HEIGHT: f64 = WAVE_HEIGHT + WAVE_PADDING_TOP + WAVE_PADDING_BOTTOM;
 
-static mut WAVE_OFFSET: f64 = WAVE_PERIOD_WIDTH * 2.0;
-static mut WAVE_WIDTH: f64 = WAVE_PERIOD_WIDTH * 5.0;
-static mut LANE_WIDTH: f64 = unsafe {WAVE_OFFSET + WAVE_WIDTH};
+const DEFAULT_WAVE_OFFSET: f64 = WAVE_PERIOD_WIDTH * 1.5;
+const MIN_WAVE_WIDTH: f64 = WAVE_PERIOD_WIDTH * 4.0;
+const MIN_LANE_WIDTH: f64 = DEFAULT_WAVE_OFFSET + MIN_WAVE_WIDTH;
 
-pub struct Compositor {}
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Compositor{
+    config: Option<&'static str>
+}
 
+impl Compositor {
+
+    pub fn new() -> Self { Self::default() }
+    pub fn with(&mut self, config: &'static str) -> &mut Self {
+        self.config = Some(config);
+        self
+    }
+
+    pub fn compose(&self, diag: &Diagram) -> svg::Document {
+        // calc document width an height
+        let max_wave_width = get_max_wave_len(diag) as f64 * WAVE_PERIOD_WIDTH;
+        let width = max_wave_width + DEFAULT_WAVE_OFFSET + PADDING * 2.0;  // todo!() calc correct wave offset.
+        let height = diag.lane_count() as f64 * LANE_HEIGHT + HEADER_HEIGHT + FOOTER_HEIGHT + PADDING * 2.0;
+        
+        println!("Document size is {}x{}",width,height);
+        println!("Longest wave found is {} periods", get_max_wave_len(diag));
+
+
+        let bg = Rectangle::new()
+            .set("id", "background")
+            .set("fill", diag.background().to_string())
+            .set("stroke", "none")
+            .set("width", "100%")
+            .set("height", "100%");
+
+        let title = Label::new(diag.title());
+        let title = Text::from(&title).translate(width/2.0,PADDING);
+
+        let mut lanes = Group::new().set("id", "lanes");
+
+        for (num,lane) in diag.lanes().iter().enumerate() {
+            lanes.append(self.compose_lane(num, lane, max_wave_width).translate(0.0, num as f64 * LANE_HEIGHT));
+        }
+
+
+        svg::Document::new()
+            .set("viewBox", (0,0,width,height))
+            .add(bg)
+            .add(title)
+            .add(lanes.translate(PADDING, PADDING + HEADER_HEIGHT))
+            
+    }
+
+    fn compose_lane(&self, num: usize, lane: &Lane, max_ww: f64) -> Group {
+        println!("Compose lane-{}",num);
+        // tile and y-axis label goes to the left
+        // wave starts at wave_offset and goes till the end.
+        let mut group = Group::new()
+            .set("id", format!("lane-{}",num));
+
+        let wave_offset = DEFAULT_WAVE_OFFSET;
+        let wave_end = wave_offset + max_ww; // max_ww gets calculated at the top of consume().
+
+        // compose y-axis labels
+        let y_axis_label_heigh = Text::from(&Label::small(lane.sig.y_axis.0))
+            .translate(wave_offset-10.0, WAVE_PADDING_TOP);
+        let y_axis_label_low = Text::from(&Label::small(lane.sig.y_axis.1))
+            .translate(wave_offset-10.0, WAVE_PADDING_TOP + WAVE_HEIGHT );
+
+
+            // add posibility to crate a label from the title.
+        let signal_name_label = Text::from(&signal_title_to_label(lane.sig.name, lane.sig.color)
+            ).translate(wave_offset-15.0, WAVE_PADDING_TOP + WAVE_HEIGHT/2.0);
+
+        group.append(y_axis_label_heigh);
+        group.append(y_axis_label_low);
+        group.append(signal_name_label);
+
+        // compose dashed lane level lines
+        group.append(self.compose_lane_level_lines(wave_offset, wave_end).set("id", format!("lane-level-lines-{}",num)));
+
+        group.append(Path::from(&lane.sig)
+            .set("id", format!("lane-{}-wave",num))
+            .translate(wave_offset, WAVE_PADDING_TOP)
+        );
+
+
+        group
+    }
+
+    fn compose_lane_level_lines(&self, start: f64 , end: f64) -> Group {
+        Group::new()
+            .add(h_dashed_line(start, end, 0.0))
+            .add(h_dashed_line(start, end, WAVE_HEIGHT/2.0))
+            .add(h_dashed_line(start, end, WAVE_HEIGHT))
+            .translate(0.0, WAVE_PADDING_TOP)
+    }
+}
+
+// helper functions
+fn get_max_wave_len(diagram: &Diagram) -> usize {
+    if let Some(max_sig) = diagram.lanes().iter().map(|l| &l.sig ).max() {
+        return max_sig.wave.len();
+    } else {
+        return 0;
+    }
+}
+
+fn h_dashed_line(x1: f64,x2: f64,y: f64) -> Line {
+    Line::new()
+        .set("stroke", Color::Lightgray.to_string())
+        .set("stroke-linecap", "round")
+        .set("stroke-width", "0.5")
+        .set("stroke-dasharray", "10 6")
+        .set("x1", x1).set("y1", y)
+        .set("x2", x2).set("y2", y)
+}
+
+fn signal_title_to_label(title: &'static str, color: Color) -> Label{
+    let mut label = Label::new(title);
+    label.set_anchor(TextAnchor::End);
+    label.set_color(color);
+    label
+}
+
+
+// Impl from model to svg elements. 
+
+impl From<&Label> for Text {
+    fn from(label: &Label) -> Text{
+        Text::new()
+            .set("fill", label.color().to_string())
+            .set("font-family", "Segoe Print")
+            .set("font-size", label.size().to_string())
+            .set("text-anchor", label.anchor().to_string())
+            .add(svg::node::Text::new(label.text()))
+    }
+}
+
+impl From<&Signal> for Path {
+    fn from(signal: &Signal) -> Self {
+        Path::new()
+        .set("fill", "none")
+        .set("stroke", signal.color.to_string())
+        .set("stroke-width", 3)
+        .set("stroke-linejoin","round")
+        .set("stroke-linecap", "round")
+        .set("d", Data::from(signal))
+    }
+}
+
+impl From<&Signal> for Data {
+    // compose svg paths data from a signal
+    fn from(sig: &Signal) -> Self {
+        // Set start conditions. 
+        let mut prev_value = match sig.wave.levels.first() {
+            Some(Level::High) => Level::High,
+            Some(Level::Down) => Level::High,
+            _ => Level::Low,
+        };
+
+        //log::debug!("First value is {:?}", prev_value);
+        
+        let y = match prev_value {
+            Level::Low => WAVE_HEIGHT,
+            Level::High => 0.0,
+            _ => WAVE_HEIGHT/2.0,
+        };
+
+        //log::debug!("Set y to {}", y);
+
+        let mut data = Data::new()
+            .move_to((0,y))
+            .horizontal_line_by(sig.phase * WAVE_PERIOD_WIDTH);
+
+            for value in sig.wave.levels.iter() {
+                if &prev_value == value {
+                    data = data.horizontal_line_by(WAVE_PERIOD_WIDTH  * sig.period);
+                } else {
+                    match value {
+                        Level::Low => {
+                            data = data
+                            .vertical_line_by(WAVE_HEIGHT)
+                            .horizontal_line_by(WAVE_PERIOD_WIDTH  * sig.period);
+                            prev_value = Level::Low;
+                        }
+                        Level::High => {
+                            data = data
+                            .vertical_line_by(-(WAVE_HEIGHT))
+                            .horizontal_line_by(WAVE_PERIOD_WIDTH * sig.period);
+                            prev_value = Level::High;
+                        }
+                        Level::Up => {
+                            data = data
+                                .vertical_line_to(WAVE_HEIGHT)
+                                .horizontal_line_by((WAVE_PERIOD_WIDTH/2.0)  * sig.period)
+                                .vertical_line_by(-(WAVE_HEIGHT ))
+                                .horizontal_line_by((WAVE_PERIOD_WIDTH/2.0)  * sig.period);
+                            prev_value = Level::High;
+                        }
+                        Level::Down => {
+                            data = data
+                                .vertical_line_to(0)
+                                .horizontal_line_by((WAVE_PERIOD_WIDTH/2.0)  * sig.period)
+                                .vertical_line_by(WAVE_HEIGHT)
+                                .horizontal_line_by((WAVE_PERIOD_WIDTH/2.0)  * sig.period);
+                            prev_value = Level::Low;
+                        }
+                        _ => { unimplemented!()}
+                    }
+                }            
+            }
+        data
+    }
+}
+
+// Transformable
+
+pub trait Transformable
+{
+    fn translate(self, x:f64, y:f64) -> Self;
+}
+
+impl Transformable for Group {
+    fn translate(self, x:f64, y:f64) -> Self {
+        self.set("transform", format!("translate({},{})",x,y))
+    }
+}
+
+impl Transformable for Path {
+    fn translate(self, x:f64, y:f64) -> Self {
+        self.set("transform", format!("translate({},{})",x,y))
+    }
+}
+
+impl Transformable for Text {
+    fn translate(self, x:f64, y:f64) -> Self {
+        self.set("transform", format!("translate({},{})",x,y))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,29 @@
 
-use siggi::model::{Signal, signal::{Wave, Clock, SignalGenerator}, Diagram, Lane, markers::{Label, Line}};
+use siggi::{model::{Signal, signal::{Wave, Clock, SignalGenerator, Level}, Diagram, Lane, markers::{Label, Line}, utils::Color}, compose::Compositor};
 
 fn main() {
-    println!("Hello, world!");
-
-    let s1 = Signal::default().name("Test").clone();
-    let mut s2= Signal::default();
-    s2.name("ts2").wave(Wave::default()).phase(0.2).period(1.5);
+    println!("Hello, siggi!");
 
     let nclk = Clock::negativ(4);
     let pclk = Clock::positiv(4).name("tclk");
+    let nclk = nclk.to_signal();
 
-    let s3 = nclk.to_signal();
+    let mut s1= Signal::default();
+    s1.name("ts 1").wave(Wave{ levels: vec![Level::Low,Level::High,Level::Low,Level::High]}).phase(0).period(1).color(Color::Yellow);
 
-    println!("{:?}",s1);
-    println!("{:?}",s2);
-    println!("{:?}",s3);
-    println!("{:?}",nclk);
-    println!("{:?}",pclk); 
+    let mut d1 = Diagram::new("Simple siggi diagram");
 
-    let mut d1 = Diagram::new("title");
+    d1.add(Lane::new(nclk).add_label(Label::default()).clone());
+    d1.add(Lane::new(s1).add_marker(Line::default()).clone());
 
-    d1.add(Lane::new(s1).add_label(Label::default()).clone());
-    d1.add(Lane::new(s2).add_marker(Line::default()).clone());
-    d1.add(Lane::new(s3).add_label(Label::default()).add_marker(Line::default()).clone());
-    let mut l4 = Lane::new(pclk.to_signal());
-    l4.label_at("lab", 2.3);
-    l4.mark_at(1.8);
-    d1.add(l4);
+    let mut l3 = Lane::new(pclk.to_signal());
+    l3.label_at("lab", 2.3);
+    l3.mark_at(1.8);
+    //d1.add(l3);
 
-    dbg!(d1);
+    let comp = Compositor::default();
+    let doc = comp.compose(&d1);
+
+    svg::save("target/siggi.svg", &doc).expect("Could not save the diagram.");
+
 }

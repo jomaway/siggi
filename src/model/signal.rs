70ea054cc2e6742f71c2271419450d039b0205cf
jomaway <dev@jomaway.de>
2022-02-24 03:38:58 +0100

    Added PartialEq, Eq, PartialOrd and Ord traits to wave and signal struct

--- src/model/signal.rs
+++ src/model/signal.rs
@@ -3,7 +3,7 @@
 use super::utils::Color;
 
 
-#[derive(Debug,Clone)]
+#[derive(Debug,Clone, PartialEq)]
 pub struct Signal {
     pub name: &'static str,
     pub wave: Wave,
@@ -13,9 +13,52 @@ pub struct Signal {
     pub y_axis: (&'static str, &'static str),
 }
 
+impl Eq for Signal {}
+
+impl PartialOrd for Signal {
+    // Order the signals by their wave length.
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.wave.partial_cmp(&other.wave) 
+    }
+}
+
+impl Ord for Signal {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.wave.cmp(&other.wave)
+    }
+}
+
 #[derive(Debug,Clone, Default)]
 pub struct Wave {
     pub levels: Vec<Level>
+}
+
+impl Wave {
+    pub fn len(&self) -> usize {
+        self.levels.len()
+    }
+}
+
+impl PartialEq for Wave {
+    // Two waves are equal if the length of the wave is the same.
+    fn eq(&self, other: &Self) -> bool {
+        self.levels.len() == other.levels.len()
+    }
+}
+
+impl Eq for Wave {}
+
+impl PartialOrd for Wave {
+    // Order the waves by length.
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.levels.len().partial_cmp(&other.levels.len())
+    }
+}
+
+impl Ord for Wave {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.levels.len().cmp(&other.levels.len())
+    }
 }
 
 #[derive(Debug,Clone,Copy)]

8a3b05f35a064c8b2a2b7ed5c5597e6c0d27d0eb
jomaway <dev@jomaway.de>
2022-02-26 15:17:33 +0100

    Added some examples

---
+++ examples/test.rs
@@ -0,0 +1,49 @@
+use siggi::{compose::Compositor, model::{Diagram, utils::Color, signal::{Clock, SignalGenerator, Wave}, Signal, markers::{Label, Line}, Lane}};
+
+
+fn main() {
+    // Create an Compositor
+    let compositor = Compositor::default();
+
+    let doc = compositor.compose(&generate_test_diagram());
+    svg::save("test.svg", &doc).expect("Could not save the diagram.");
+}
+
+#[allow(unused)]
+fn generate_test_diagram() -> Diagram {
+    let nclk = Clock::negativ(4);
+    let pclk = Clock::positiv(4).name("tclk");
+    let nclk = nclk.to_signal();
+
+    let mut s1= Signal::default();
+    s1.set_name("ts 1")
+        .set_wave( "hlhlhlhlhlhlhlhlhlhl".parse::<Wave>().unwrap())
+        .set_phase(0)
+        .set_period(0.2)
+        .set_color(Color::Yellow);
+
+    let mut d1 = Diagram::new(Some("Simple siggi diagram".to_string()));
+
+    let markerline = Line::new(1.5,true,1.2, Color::Red);
+    d1.append(
+        Lane::new(nclk)
+            .add_marker(markerline)
+            .add_label_at("rising edge", 1.5)
+        );
+    d1.append(
+        Lane::new(s1)
+            .add_marker(markerline)
+            .add_label_at("0", 0.5)
+            .add_label_at("1", 1.5)
+            .add_label_at("0", 2.5)
+            .add_label_at("1", 3.5)
+        );
+
+    let mut l3 = Lane::new(pclk.to_signal().color_with(Color::Blue));
+    
+    l3.append_label_at(String::from("&lt;- t -&gt;"), 1.5);
+    l3.append_marker_at(1.0);
+    l3.append_marker_at(2.0);
+
+    d1.add(l3)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 
-use std::{str::FromStr, fs};
+use std::{fs};
 
-use siggi::{model::{Signal, signal::{Wave, Clock, SignalGenerator}, Diagram, Lane, markers::{Label, Line}, utils::Color}, compose::Compositor, parse::{self, }};
+use siggi::{compose::Compositor, parse};
 
 use clap::Parser as ClapParser;
 
@@ -31,9 +31,6 @@ use clap::Parser as ClapParser;
     #[clap(long)]
     tocks: bool,
 
-    #[clap(long)]
-    test: bool,
-
     #[clap(short,long)]
     input_file: Option<String>
 }
@@ -43,10 +40,6 @@ fn main() {
 
     let args = Args::parse();
     let comp = Compositor::default();
-
-    if args.test {
-        svg::save("target/test.svg",&comp.compose(&generate_test_diagram())).expect("Test failed");
-    }
 
     if let Some(path) = args.input_file {
         let data = fs::read_to_string(path).expect("Unable to read file");
@@ -58,87 +51,5 @@ fn main() {
         let doc = comp.compose(&diagram);
         svg::save(args.output, &doc).expect("Could not save the diagram.");
     }
-    // svg::save("target/test.svg", &comp.compose(&generate_manchester_example())).expect("Could not save the test diagram.");
 }
 
-#[allow(unused)]
-fn generate_test_diagram() -> Diagram {
-    let nclk = Clock::negativ(4);
-    let pclk = Clock::positiv(4).name("tclk");
-    let nclk = nclk.to_signal();
-
-    let mut s1= Signal::default();
-    s1.set_name("ts 1")
-        .set_wave( "hlhlhlhlhlhlhlhlhlhl".parse::<Wave>().unwrap())
-        .set_phase(0)
-        .set_period(0.2)
-        .set_color(Color::Yellow);
-
-    let mut d1 = Diagram::new(Some("Simple siggi diagram".to_string()));
-
-    let markerline = Line::new(1.5,true,1.2, Color::Red);
-    d1.append(
-        Lane::new(nclk)
-            .add_marker(markerline)
-            .add_label_at("rising edge", 1.5)
-        );
-    d1.append(
-        Lane::new(s1)
-            .add_marker(markerline)
-            .add_label_at("0", 0.5)
-            .add_label_at("1", 1.5)
-            .add_label_at("0", 2.5)
-            .add_label_at("1", 3.5)
-        );
-
-    let mut l3 = Lane::new(pclk.to_signal().color_with(Color::Blue));
-    
-    l3.append_label_at(String::from("&lt;- t -&gt;"), 1.5);
-    l3.append_marker_at(1.0);
-    l3.append_marker_at(2.0);
-
-    d1.add(l3)
-}
-
-#[allow(unused)]
-fn generate_manchester_example() -> Diagram {
-    let clk = Clock::negativ(11);
-    let clk = clk.to_signal().color_with(Color::Yellow);
-
-    let mut s1= Signal::new("Data", Wave::from_str("hlhllhhhllh").unwrap() );
-    s1.set_color(Color::Red);
-
-    let mut s2 = Signal::new("Manchester",Wave::from_str("ududduuuddu").unwrap());
-    s2.set_color(Color::Blue);
-
-    let labels = vec![
-        Label::from("1").at(0.5),
-        Label::from("0").at(1.5),
-        Label::from("1").at(2.5),
-        Label::from("0").at(3.5),
-        Label::from("0").at(4.5),
-        Label::from("1").at(5.5),
-        Label::from("1").at(6.5),
-        Label::from("1").at(7.5),
-        Label::from("0").at(8.5),
-        Label::from("0").at(9.5),
-        Label::from("0").at(10.5)
-    ];
-
-    let markerline = Line::new(1.5,true,1.2, Color::Red);
-
-    let mut d1 = Diagram::new(Some("Manchester Encoding".to_string()));
-
-    let l1 = Lane::new(clk).add_marker(markerline).clone();
-    let mut l2 = Lane::new(s1).add_marker(markerline).clone();
-    let mut l3 = Lane::new(s2).add_marker(markerline).clone();
-        
-    l2.labels.extend(labels.clone());
-    l3.labels.extend(labels);
-
-    d1.append(l1);
-    d1.append(l2);
-    d1.append(l3);
-
-    d1
-}
